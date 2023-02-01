fb8b49bd15a9b291b571352080a4077a837bd431
Alex Kirhenshtein <alk@netxms.org>
2023-02-01 16:04:38 +0200

    cosmetic changes

--- docker/build.sh
+++ docker/build.sh
@@ -2,6 +2,16 @@
 
 set -e
 
-mock -r rocky+epel-8-$(arch) --spec SPECS/*.spec --sources SOURCES --addrepo https://packages.netxms.org/epel/8/$(arch)/stable
-mock -r rocky+epel-9-$(arch) --spec SPECS/*.spec --sources SOURCES --addrepo https://packages.netxms.org/epel/9/$(arch)/stable
+for V in 8 9; do
+   mock -r rocky+epel-$V-$(arch) --spec SPECS/*.spec --sources SOURCES \
+      --addrepo https://packages.netxms.org/epel/$V/$(arch)/stable \
+      --addrepo https://packages.netxms.org/devel/epel/$V/$(arch)/stable
+done
+
+for V in 36 37; do
+   mock -r fedora-$V-$(arch) --spec SPECS/*.spec --sources SOURCES \
+      --addrepo https://packages.netxms.org/fedora/$V/$(arch)/stable \
+      --addrepo https://packages.netxms.org/devel/fedora/$V/$(arch)/stable
+done
+
 cp /var/lib/mock/*/result/*.rpm /result/
